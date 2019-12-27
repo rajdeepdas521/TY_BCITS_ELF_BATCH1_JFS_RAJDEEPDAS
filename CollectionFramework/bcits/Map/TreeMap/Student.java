04d0173dadf9af595939e564c0721e4ae69c81cb
Rajdeep Das <rajdeepdas842@gmail.com>
2019-12-27 14:32:46 +0530

    hello world

---
+++ CollectionFramework/bcits/Map/TreeMap/Student.java
@@ -0,0 +1,19 @@
+package com.bcits.Map.TreeMap;
+
+public class Student
+{
+int age;
+String name;
+public Student(int age, String name)
+{
+	super();
+	this.age = age;
+	this.name = name;
+}
+@Override
+public String toString() {
+	return "Student [age=" + age + ", name=" + name + "]";
+}
+
+
+}
